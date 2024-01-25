6ecfcfc234ac725eab3c5a34dac263bf28b3c350
ronykax <ronykax@proton.me>
2024-01-25 17:09:45 +0530

    more projects

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,14 +4,23 @@ import Link from "./Link.svelte";
 import X from "./assets/X.svg?raw";
 import Discord from "./assets/Discord.svg?raw";
 import Github from "./assets/Github.svg?raw";
-import Youtube from "./assets/Youtube.svg?raw";
 import Linked from "./assets/Linked.svg?raw";
-// import Online from "./assets/Online.svg?raw";
-// import Offline from "./assets/Offline.svg?raw";
-// import Idle from "./assets/Idle.svg?raw";
-// import Dnd from "./assets/Dnd.svg?raw";
 
-const config = {
+interface Config {
+    socials: {
+        url: string;
+        icon: string;
+    }[],
+    projects: {
+        title: string;
+        description: string;
+        url: string;
+        icon: string;
+    }[],
+    discordUid: string;
+}
+
+const config: Config = {
     socials: [
         {
             url: "https://x.com/ronykax",
@@ -24,16 +33,8 @@ const config = {
         {
             url: "https://github.com/ronykax",
             icon: Github
-        },
-        {
-            url: "/",
-            icon: Youtube
         }
-    ] as {
-        url: string;
-        icon: string;
-    }[],
-
+    ],
     projects: [
         {
             title: "Netcord",
@@ -49,19 +50,24 @@ const config = {
         },
         {
             title: "Math Run",
-            description: "Simple game where you speedrun through problems math problems",
+            description: "Beat the clock or race against it and speedrun through math problems",
             url: "https://github.com/ronykax",
             icon: "MathRun.png"
+        },
+        {
+            title: "ChibeeU",
+            description: "Become a chibee-like you! A free and cute avatar editor (soon)",
+            url: "https://chibeeu.com",
+            icon: "Folio.png"
+        },
+        {
+            title: "Untitled Web",
+            description: "A group of creatives offering web and app development services",
+            url: "/",
+            icon: "Folio.png"
         }
-    ] as {
-        title: string;
-        description: string;
-        url: string;
-        icon: string;
-    }[],
-
-    discordUID: "791222882499690519"
+    ],
+    discordUid: "791222882499690519"
 }
 
-export { X, Discord, Github, Youtube, Linked, Link, config }
-// export { X, Discord, Github, Youtube, Linked, Link, Online, Offline, Idle, Dnd, config }
+export { Linked, Link, config }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
     import { slide, blur } from "svelte/transition";
-    import { Linked, Link, config } from "$lib";
-    // import { Linked, Link, Online, Offline, Idle, Dnd, config } from "$lib";
     import { useLanyard } from "sk-lanyard";
-
-    type status = "online" | "offline" | "dnd" | "idle";
+    import { Linked, Link, config } from "$lib";
 
     let seeMore = false;
-    const lanyard = useLanyard({ method: "ws", id: config.discordUID });
-
-    $: presence = $lanyard && $lanyard.discord_status as status;
+    const lanyard = useLanyard({ method: "ws", id: config.discordUid });
 </script>
 
 <div class="max-w-screen-md mx-auto bg-blue.gray px-6 py-6 space-y-12 md:py-20">
@@ -20,32 +15,22 @@
             
             <div>
                 <p>
-                    I'm 15y/o building cool stuff since 2018. I mainly use <Link href="https://www.typescriptlang.org/">TypeScript</Link> and I'm currently learning <Link href="https://www.rust-lang.org/">Rust</Link>.
+                    I'm 15y/o building cool stuff since 2018. I mainly use <Link href="https://www.typescriptlang.org/">TypeScript</Link> and I'm currently learning <Link href="https://www.rust-lang.org/">Rust</Link>. I'm open to new opportunities!
                 </p>
 
                 {#if seeMore}
                     <p transition:slide={{ duration: 100 }}>
                         <br>
-                        Aside from programming, I also enjoy watching anime. Some of my favorite animes are <Link href="https://google.com/search?q=attack+on+titan">Attack On Titan</Link> and <Link href="https://google.com/search?q=demon+slayer">Demon Slayer</Link>. <Link href="https://google.com/search?q=rocket+league">Rocket League</Link> is my ideal game to play and I'm into aviation too!
+                        Aside from programming, I also enjoy watching anime. Some of my favorite animes are <Link href="https://www.imdb.com/title/tt2560140/">Attack On Titan</Link> and <Link href="https://www.imdb.com/title/tt9335498/">Demon Slayer</Link>. <Link href="https://www.rocketleague.com/">Rocket League</Link> is my ideal game to play and I'm into aviation too!
                     </p>
                 {/if}
 
-                <button class="mt-2 font-medium text-white text-opacity-50 underline underline-offset-4 hover:text-opacity-75" on:click={() => seeMore = !seeMore}>{seeMore ? "See less" : "More about me"}</button>
+                <button class="mt-2 font-medium text-white text-opacity-50 underline underline-offset-4 hover:text-opacity-75" on:click={() => seeMore = !seeMore}>{seeMore ? "See less" : "Learn more"}</button>
             </div>
-
-            <ul class="flex flex-wrap gap-4 mt-6">
-                {#each config.socials as item}
-                    <li>
-                        <a href="{item.url}" class="{item.url}">
-                            <span class="opacity-75 hover:opacity-100">{@html item.icon}</span>
-                        </a>
-                    </li>
-                {/each}
-            </ul>
         </div>
         
         <div class="p-1 rounded-full ring-1 ring-white ring-opacity-25 max-w-32 max-h-32">
-            <img class="w-full h-full rounded-full" src="chibee.jpg" width="400" height="400" alt="">
+            <img class="w-full h-full rounded-full" src="chibee.jpg" alt="">
         </div>
     </div>
 
@@ -53,7 +38,7 @@
 
         {#each config.projects as item}
             <li>
-                <a href="{item.url}">
+                <a href="{item.url}" target="_blank">
                     <div class="h-full p-4 bg-white rounded-[4px] bg-opacity-5 hover:bg-opacity-7 ring-1 ring-white ring-opacity-15 backdrop-blur-sm space-y-1">
                         
                         <h2 class="font-bricolage.grotesque relative">
@@ -71,22 +56,38 @@
 
     </ul>
 
-    <div>
-        {#if presence}
-            <div transition:slide={{ duration: 500 }}>
-                <h2 class="text-opacity-25" transition:blur={{ duration: 500 }}>
-                    Currently
-                    <a href="https://discord.com/users/{config.discordUID}">
-                        <span class="px-1.5 py-0.5 opacity-75 bg-white rounded-md bg-opacity-7 font-mono relative hover:opacity-100">
-                            <img class="inline-block" src="{presence}.svg" alt="">
-                            {presence}
+    <div class="flex flex-col gap-6 md:flex-row md:justify-between md:items-center md:gap-0">
+        <div>
+            {#if $lanyard}
+                <div transition:slide={{ duration: 250 }}>
+                    <h2 class="text-opacity-25" transition:blur={{ duration: 500 }}>
+                        Currently
+    
+                        <a href="https://github.com/Phineas/lanyard">
+                            <span class="px-1.5 py-0.5 opacity-75 bg-white rounded-md bg-opacity-7 font-mono relative hover:opacity-100">
+                                <img class="inline-block animate-pulse" src="{$lanyard.discord_status}.svg" alt="">
+                                {$lanyard.discord_status}
+                            </span>
+                        </a>
+    
+                        on Discord.
+                    </h2>
+                </div>
+            {/if}
+    
+            <h2 class="text-opacity-25">© 2024 — All rights reserved</h2>
+        </div>
+
+        <ul class="flex flex-wrap gap-3">
+            {#each config.socials as item}
+                <li>
+                    <a class="{item.url}" href="{item.url}" target="_blank">
+                        <span class="opacity-25 hover:opacity-75">
+                            {@html item.icon}
                         </span>
                     </a>
-                    on Discord.
-                </h2>
-            </div>
-        {/if}
-
-        <h2 class="text-opacity-25">© 2024 — All rights reserved</h2>
+                </li>
+            {/each}
+        </ul>
     </div>
 </div>
