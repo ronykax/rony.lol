c053cfb38cd2de0209f5a7d57c8eec879c248b5b
ronykax <ronykax@proton.me>
2024-02-03 16:41:20 +0000

    specify w & h atttributes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,7 +30,7 @@
         </div>
         
         <div class="p-1 rounded-full ring-1 ring-white ring-opacity-25 max-w-32 max-h-32">
-            <img class="w-full h-full rounded-full" src="chibee.webp" alt="">
+            <img class="w-full h-full rounded-full" src="chibee.webp" alt="Rony's Chibee" width="400" height="400">
         </div>
     </div>
 
